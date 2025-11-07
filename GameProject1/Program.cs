35bdf0d7dd93bd221824fdabbf13643e99d93797
Sophie <kbsully2019@gmail.com>
2025-11-07 11:33:59 +0000

    add gameproject1

---
+++ GameProject1/Program.cs
@@ -0,0 +1,14 @@
+﻿class Human(string pName, int pAge, string pProfession, string pStartingLocation, int pSpeed, int pDefaultHealth) {
+	public string Name = pName;
+	private string Age = pAge;
+	private string Profession = pProfession;
+	private string StartingLocation = pStartingLocation;
+	private int Money = 0;
+	private int Speed = pSpeed;
+	private int Health = pDefaultHealth;
+
+	public void Dialogue(string Dialogue) {
+		Console.WriteLine("${Name} says  {Dialogue}")
+	}
+	
+}
